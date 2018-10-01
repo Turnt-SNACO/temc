d3b48b5101fc764926c4418dd9500967996a7b0f
james <janderson5@albany.edu>
2018-10-01 19:46:07 -0400

    removed some extra whitespaces

--- server/Temc/src/main/java/receive/Receiver.java
+++ server/Temc/src/main/java/receive/Receiver.java
@@ -2,38 +2,19 @@ package receive;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
-
 import com.google.gson.*;
 
 public class Receiver {
-			
 		public static void main(String args[]) {
-
 			String jsonStr = "{ \"rec\": \"rob\", \"msg\": \"poop\" }";
-			
 			new Receiver().work(jsonStr);
 			//System.out.println(temp);
 		}
-	
-	
 		public String work(String jsonStr) {
-			
 			Gson gson = new Gson();
 			System.out.println(jsonStr);
 			DataIn in = gson.fromJson(jsonStr, DataIn.class);
-			
-			
 			System.out.println(in.getMsg());
-			
 			return "sent sent";
-			
-			
-			
-			
-			
 		}
-		
-	
-	
-
 }
