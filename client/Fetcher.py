a999d6c1b1c88a25b46a6f92b75e6458b51e587c
james <janderson5@albany.edu>
2018-10-01 19:28:58 -0400

    a pretty intrusive message fetching script

---
+++ client/Fetcher.py
@@ -0,0 +1,30 @@
+import time
+from subprocess import Popen, PIPE, STDOUT, call
+import sys
+import os
+URL = "http://nascloud.myds.me:8080/Temc/main"
+uname = ""
+exists = os.path.isfile('.temcrc')
+if exists:
+    f = open('.temcrc',"r")
+    contents = f.read()
+    #print(contents)
+    lines = contents.split()
+    for s in lines:
+        if 'uname' in s:
+            uname = s[6:] 
+else:    
+    uname = input("You do not have a user profile.  Enter a username:")
+    f = open('.temcrc',"w+")
+    f.write('uname='+uname)
+pid = os.fork()
+if pid == 0:
+    for i in range(5):
+        time.sleep(1)
+        cmd = 'curl -s -d "'+uname+'" -X POST "'+URL+'"'
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+        call(['clear'])
+        print("New message:", output.decode('UTF-8'))
+else:
+    print('parent done')
