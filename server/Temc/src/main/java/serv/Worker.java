724741605aafe8d73752dfb67ceae17b854c84ec
james <janderson5@albany.edu>
2018-10-01 20:02:42 -0400

    fixed spagetti

--- server/Temc/src/main/java/serv/Worker.java
+++ server/Temc/src/main/java/serv/Worker.java
@@ -1,33 +1,15 @@
 package serv;
-//package proj3;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
 import com.google.gson.*;
 
 public class Worker {
-			
-		
-	
-	
-	
-		public String work(String jsonStr) {
-			
-			Gson gson = new Gson();
-			System.out.println(jsonStr);
-			//InList dataIn = gson.fromJson(jsonStr, InList.class);
-			
-			//System.out.println(dataIn.getInList(0));
-			
-			return "message got";
-			
-			
-			
-			
-			
-		}
-		
-	
-	
-
+	public String work(String jsonStr) {
+		Gson gson = new Gson();
+		System.out.println(jsonStr);
+		//InList dataIn = gson.fromJson(jsonStr, InList.class);
+		//System.out.println(dataIn.getInList(0));
+		return "message got";
+	}
 }
