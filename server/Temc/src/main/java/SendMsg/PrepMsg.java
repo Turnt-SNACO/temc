54e3452fa31c95c8a2af70fa3ba4fee4e6d42013
james <janderson5@albany.edu>
2018-10-02 15:30:21 -0400

    updated sendmsg package to comply with new json format

--- server/Temc/src/main/java/SendMsg/PrepMsg.java
+++ server/Temc/src/main/java/SendMsg/PrepMsg.java
@@ -6,23 +6,17 @@ import java.util.Hashtable;
 import com.google.gson.*;
 
 public class PrepMsg {
-	
-//		public static void main(String args[]) {
-//			String jsonStr = "{ \"rec\": \"rob\", \"msg\": \"poop\" }";
-//			new PrepMsg().work(jsonStr);
-//			//System.out.println(temp);
-//		}
-	
-		public String prep(String jsonStr,String realPath) {
-			Gson gson = new Gson();
-			System.out.println(jsonStr);
-			DataOut message = gson.fromJson(jsonStr, DataOut.class);
-			try{
-				File file = new File(realPath+"/messages/"+message.getRec());
-				BufferedReader br = new BufferedReader(new FileReader(file));
-				return br.readLine();
-			} catch (Exception e) {
-				return null;
-			}
+	public String prep(String jsonStr,String realPath) {
+		Gson gson = new Gson();
+		System.out.println(jsonStr);
+		Request request = gson.fromJson(jsonStr, Request.class);
+		String uname = Request.getUname();
+		try{
+			File file = new File(realPath+"/messages/"+uname);
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			return br.readLine();
+		} catch (Exception e) {
+			return null;
 		}
+	}
 }
---
+++ server/Temc/src/main/java/SendMsg/Request.java
@@ -0,0 +1,13 @@
+package SendMsg;
+
+public class Request {
+	private String uname;
+	private String pass;
+	public String getUname() {
+		return uname;
+	}
+	public String getPass() {
+		return pass;
+	}
+
+}
