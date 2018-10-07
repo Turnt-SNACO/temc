45696871d4092e4c00f559ea275759b9b78d28b2
james <janderson5@albany.edu>
2018-10-06 20:09:52 -0400

    delete already read lines

--- server/Temc/src/main/java/SendMsg/PrepMsg.java
+++ server/Temc/src/main/java/SendMsg/PrepMsg.java
@@ -1,11 +1,32 @@
 package SendMsg;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Hashtable;
 import com.google.gson.*;
+import java.util.Scanner;
+
 
 public class PrepMsg {
+	//debugging
+	// public static void main(String[] args){
+	// 	try{
+	// 		File file = new File(System.getProperty("user.dir")+"/test");
+	// 		Scanner fs = new Scanner(file);
+	// 		fs.nextLine();
+	// 		//rewrite file without the message it took out
+	// 		FileWriter fw = new FileWriter(file);
+	// 		BufferedWriter bw = new BufferedWriter(fw);
+	// 		while(fs.hasNextLine()) {
+	// 			String next = fs.nextLine();
+	// 			bw.write(next);
+	// 			bw.newLine();
+	// 		}
+	// 		bw.close();
+	// 		fs.close();
+	// 	}
+	// 	catch (IOException e){
+	// 		System.out.println("Error opening file");
+	// 	}
+	// }
 	public String prep(String jsonStr,String realPath) {
 		Gson gson = new Gson();
 		System.out.println(jsonStr);
@@ -13,8 +34,17 @@ public class PrepMsg {
 		String uname = request.getUname();
 		try{
 			File file = new File(realPath+"/messages/"+uname);
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			return br.readLine();
+			Scanner fs = new Scanner(file);
+			message = fs.nextLine();
+			//rewrite file without the message it took out
+			BufferedWriter bw = new BufferedWriter(file);
+			FileWriter fw = new FileWriter(file);
+			while(fs.hasNextLine()) {
+				String next = fs.nextLine();
+				bw.write(next);
+				bw.newLine();
+			}
+			return message;
 		} catch (Exception e) {
 			return null;
 		}
--- server/Temc/src/main/java/SendMsg/Request.java
+++ server/Temc/src/main/java/SendMsg/Request.java
@@ -9,5 +9,4 @@ public class Request {
 	public String getPass() {
 		return pass;
 	}
-
 }
